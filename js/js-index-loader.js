e43a3ecee59ceeaac4ba9761a36c03ec40d27e30
Leonardo-Rocca <leonardo_7@live.com.ar>
2020-06-07 15:25:59 -0300

    try performance

--- js/js-index-loader.js
+++ js/js-index-loader.js
@@ -1,7 +1,7 @@
 [
     'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
     'js/main.min.js',
-    'js/index-post-jq.js'
+   // 'js/index-post-jq.js'
 ].forEach(function(src) {
     var script = document.createElement('script');
     script.src = src;
